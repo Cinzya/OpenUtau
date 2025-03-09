50e30ef800a79deb4f1dbc686494f49677e6eb47
Cynthia Ebert <cynti-ebert@web.de>
2025-03-09 04:00:33 +0100

    fix(g2p): update training configuration for German dataset

--- py/g2p/train.py
+++ py/g2p/train.py
@@ -31,25 +31,25 @@ if __name__ == '__main__':
     from g2p.model import GreedyG2p
 
     # The config specifying grapheme set and phoneme set.
-    cfg = OmegaConf.load('g2p/en_us/cfg.yaml')
+    cfg = OmegaConf.load('g2p/de_DE/cfg.yaml')
 
     # Loads the dataset.
     # Note that SphinxDataset may not work for your dictionary format.
-    dataset = SphinxDataset('g2p/en_us/cmudict-0.7b', cfg,
+    dataset = SphinxDataset('g2p/de_DE/dict.txt', cfg,
                             comment_prefix=';;;',
                             # "RECORDS(1)" -> "RECORDS"
-                            remove_word_digits=True,
+                            remove_word_digits=False,
                             # "R EH1 K ER0 D Z" -> "R EH K ER D Z"
-                            remove_phoneme_digits=True)
+                            remove_phoneme_digits=False)
 
     # Create trainer. You may need to adjust the batch size and epochs.
     trainer = G2pTrainer(
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-        loss_device=torch.device("cpu"),
+        loss_device=torch.device("cuda"),
         model=hydra.utils.instantiate(cfg),
         dataset=dataset,
-        batch_size=256,
-        epochs=10)
+        batch_size=16,
+        epochs=300)
 
     train(trainer)
 
